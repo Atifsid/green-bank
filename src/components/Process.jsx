41fe0bcb6f8982664ddd2c41a83ccbfe581e9f61
Atifsid <atifsiddiqui35@gmail.com>
2025-03-03 13:11:30 +0530

    fix div order

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -49,7 +49,7 @@ function Process() {
                 </div>
 
                 <div className='flex lg:flex-row flex-col justify-between items-center mt-24 lg:mt-32'>
-                    <div className='transform-3d flex flex-col order-1 lg:order-2'>
+                    <div className='transform-3d flex flex-col'>
                         <CreditCard
                             iconColor={'white'}
                             labelClass={'text-[#ffffff]'}
@@ -70,7 +70,7 @@ function Process() {
                             cardSignClass={'h-[23px] w-[29px] lg:h-[46px] lg:w-[58px]'}
                             style={'rotate-x-40 -rotate-z-8 rotate-y-22 h-[250px] w-[280px] sm:h-[274px] sm:w-[400px] lg:h-[274px] lg:w-[435px] bg-primary -mt-25 border-2 border-gray -ml-5'} />
                     </div>
-                    <div className='w-[80%] lg:w-[50%] order-2 lg:order-1 lg:mt-8'>
+                    <div className='w-[80%] lg:w-[50%] lg:mt-8'>
                         <h6 className='ff-m-plus-1-bold text-white lg:text-[40px]/14 text-[30px]/10 text-center lg:text-left'>Find the Perfect Credit Card<br /> for You</h6>
                         <h6 className='ff-m-plus-1-regular text-gray text-[16px]/5.5 mt-4 mb-6 text-center lg:text-left'>Discover your ideal credit card with ease. Our comprehensive selection caters to every financial need and lifestyle. Whether you seek cashback rewards, travel perks, or building credit, we have the perfect credit card waiting for you. Unleash the possibilities and find the credit card that fits your unique goals and aspirations.</h6>
                         <div className='flex justify-center lg:justify-start'>
