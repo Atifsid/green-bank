24a5b076b5a6c61a74af2f9562875a2aa621f626
Atifsid <atifsiddiqui35@gmail.com>
2025-03-03 23:03:23 +0530

    make logos slide infinitely

--- src/components/ui/LogoSection.jsx
+++ src/components/ui/LogoSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import dell from '/dell.svg'
 import nyt from '/nyt.svg'
 import petal from '/petal.svg'
@@ -6,20 +6,38 @@ import rakuten from '/rakuten.svg'
 import upwork from '/upwork.svg'
 import vice from '/vice.svg'
 
-const images = [upwork, petal, rakuten, nyt, vice, dell];
+const images = [upwork, petal, rakuten, nyt, vice, dell].map((src) => ({
+    id: crypto.randomUUID(),
+    src
+}));
 
 function LogoSection() {
+    const logosRef = useRef(null);
+
+    useEffect(() => {
+        if (logosRef.current) {
+            const clonedNode = logosRef.current.cloneNode(true);
+            clonedNode.setAttribute("aria-hidden", "true");
+            logosRef.current.parentNode.appendChild(clonedNode);
+        }
+    }, []);
+
     return (
-        <div className='flex justify-center'>
-            <div className="overflow-hidden whitespace-nowrap relative w-6xl">
-                <div className='flex items-center gap-20'>
-                    {images.map((img, i) => (
-                        <img key={i} src={img} alt={`logo-${i}`} />
+        <div className="w-full max-w-6xl mx-auto px-4 md:px-6">
+            <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
+                <ul
+                    ref={logosRef}
+                    className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+                >
+                    {images.map((image, index) => (
+                        <li key={index}>
+                            <img src={image.src} alt={image.id} />
+                        </li>
                     ))}
-                </div>
+                </ul>
             </div>
         </div>
-    )
+    );
 }
 
 export default LogoSection
