9d7b7786f71cd5621a0e5d9c778faaccf611c90a
Atifsid <atifsiddiqui35@gmail.com>
2025-03-03 23:31:52 +0530

    apply infinite-scroll on testimonials as well

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import avatar1 from '/avatar1.png';
 import avatar2 from '/avatar2.png';
 import avatar3 from '/avatar3.png';
@@ -25,27 +25,42 @@ const testimonials = [
 ]
 
 function Testimonial() {
+    const testimonialRef = useRef(null);
+
+    useEffect(() => {
+        if (testimonialRef.current) {
+            const clonedNode = testimonialRef.current.cloneNode(true);
+            clonedNode.setAttribute("aria-hidden", "true");
+            testimonialRef.current.parentNode.appendChild(clonedNode);
+        }
+    }, []);
 
     return (
-        <section className='mt-32 flex justify-center px-6 lg:px-2'>
-            <div className='flex justify-between items-center gap-8 w-6xl'>
-                {testimonials.map((testimonial, i) => (
-                    <TestimonialView
-                        key={`${testimonial.username}-${i}`}
-                        avatar={testimonial.avatar}
-                        text={testimonial.text}
-                        username={testimonial.username}
-                        work={testimonial.work}
-                    />
-                ))}
+        <div className="mt-32 w-full max-w-6xl mx-auto px-4 md:px-6">
+            <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_10px,_black_calc(100%-10px),transparent_100%)]">
+                <ul
+                    ref={testimonialRef}
+                    className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+                >
+                    {testimonials.map((testimonial, index) => (
+                        <li key={index}>
+                            <TestimonialView
+                                avatar={testimonial.avatar}
+                                text={testimonial.text}
+                                username={testimonial.username}
+                                work={testimonial.work}
+                            />
+                        </li>
+                    ))}
+                </ul>
             </div>
-        </section>
+        </div>
     )
 }
 
 function TestimonialView({ text, username, work, avatar }) {
     return (
-        <div className='flex flex-col gap-8 bg-[#27322F3D] backdrop-blur-lg py-12 px-6 rounded-4xl'>
+        <div className='flex flex-col gap-8 bg-[#27322F3D] backdrop-blur-lg py-12 px-6 rounded-4xl min-w-[371px] min-h-[331px]'>
             <div className='p-4 bg-[#3D544D] w-fit rounded-full'>
                 <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M10.4962 4.25098C3.62469 5.58431 3 12.251 3 12.251M3 12.251V16.5029C3 17.1981 3 17.5457 3.05736 17.8348C3.2937 19.026 4.22493 19.9573 5.41618 20.1936C5.70527 20.251 6.05289 20.251 6.74812 20.251V20.251C7.44335 20.251 7.79097 20.251 8.08006 20.1936C9.2713 19.9573 10.2025 19.026 10.4389 17.8348C10.4962 17.5457 10.4962 17.1981 10.4962 16.5029V16.251C10.4962 15.321 10.4962 14.856 10.394 14.4745C10.1166 13.4392 9.30797 12.6306 8.2727 12.3532C7.8912 12.251 7.42621 12.251 6.49624 12.251H3Z" stroke="white" strokeWidth="1.5" />
